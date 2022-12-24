f638fc323f3c564f3200a15bf0540792697881f8
Emaan <saniaumer011@gmail.com>
2022-12-24 12:37:33 +0500

    added vehicles ka code

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const bcrypt = require("bcrypt");
 
-
-
 const methodover = require("method-override");
 const flash = require("connect-flash");
 const session = require("express-session");
 const { isAdmin, isLoggedIn } = require("./middleware");
-
 
 //Connection Info
 let con;
@@ -21,7 +18,7 @@ const connect = async function () {
   con = await mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "@bubakar1243",
+    password: "308NegraAroyoLane",
     database: "project",
     insecureAuth: true,
   });
@@ -83,39 +80,56 @@ app.get(
     res.render("Users/manageemployees.ejs");
   })
 );
-app.get("/routes/manage",catchAsync( async(req, res) => {
-  const [rows] = await con.execute( `select * from distancefare`);
-    res.render("Users/manageroutes.ejs",{rows});
+app.get(
+  "/routes/manage",
+  catchAsync(async (req, res) => {
+    const [rows] = await con.execute(`select * from distancefare`);
+    res.render("Users/manageroutes.ejs", { rows });
   })
 );
-app.post("/routes/add",async(req,res)=>{
-  const sql_statement1 = `INSERT INTO distancefare VALUES ` +
+app.post("/routes/add", async (req, res) => {
+  const sql_statement1 =
+    `INSERT INTO distancefare VALUES ` +
     `("${req.body.destination_name}", ${req.body.student_fare})`;
-    await con.execute(`SET FOREIGN_KEY_CHECKS=0`);
-    await con.execute(sql_statement1);
-    await con.execute(`SET FOREIGN_KEY_CHECKS=1`);
-    res.redirect("/routes/manage");
-   
-  
-})
+  await con.execute(`SET FOREIGN_KEY_CHECKS=0`);
+  await con.execute(sql_statement1);
+  await con.execute(`SET FOREIGN_KEY_CHECKS=1`);
+  res.redirect("/routes/manage");
+});
 app.get(
   "/managevehicles",
   catchAsync((req, res) => {
     res.render("Users/managevehicles.ejs");
   })
 );
-app.get("/profile", catchAsync(async (req, res) => {
+app.get(
+  "/profile",
+  catchAsync(async (req, res) => {
     let cms_id = 352147;
-    const [rows] = await con.execute( `select * from student where CmsID=${cms_id}`);
+    const [rows] = await con.execute(
+      `select * from student where CmsID=${cms_id}`
+    );
     let info = rows[0];
     res.render("Users/profile.ejs", { info });
-  }));
-app.get("/logout",(req,res)=>{
-    req.session.__id=null;
-    req.flash("success", "Logged Out Successfully");
-    res.redirect("/home");
-})
-app.post("/login", catchAsync(async (req, res) => {
+  })
+);
+app.get(
+  "/managestudents",
+  catchAsync(async (req, res) => {
+    const [rows] = await con.query(
+      `select FirstName,LastName,Email,CmsID,Phone,DestName, DriverID, Enddate from students`
+    );
+    res.render("Users/managestudents.ejs", { rows });
+  })
+);
+app.get("/logout", (req, res) => {
+  req.session.__id = null;
+  req.flash("success", "Logged Out Successfully");
+  res.redirect("/home");
+});
+app.post(
+  "/login",
+  catchAsync(async (req, res) => {
     const Email = req.body.username;
     const AccPassword = req.body.password;
     const UserRole = req.body.role;
@@ -125,7 +139,7 @@ app.post("/login", catchAsync(async (req, res) => {
     if (rows[0]) {
       const validPass = await bcrypt.compare(AccPassword, rows[0].AccPassword);
       if (validPass && rows[0].UserRole == UserRole) {
-        req.session.__id=Email;
+        req.session.__id = Email;
         req.flash("success", "Logged in Successfully");
         res.redirect("/home");
       } else {
@@ -139,36 +153,43 @@ app.post("/login", catchAsync(async (req, res) => {
   })
 );
 
-app.get("/register",isLoggedIn,isAdmin,(req, res) => {
-   res.render("Users/register.ejs");
+app.get("/register", isLoggedIn, isAdmin, (req, res) => {
+  res.render("Users/register.ejs");
 });
-app.get("/vehicle_register",async(req,res)=>{
-      const [rows] = await con.execute(`select * from vehicle`);
-      // for(let row of rows){
-      //   console.log(row.Color)
-      // }
-      res.render("vehicles/vehicle_register.ejs",{rows})
-})
-app.post("/vehicle_register",async(req,res)=>{
-  const sql_statement1 = `INSERT INTO vehicle VALUES ` +
-  `("${req.body.plate_no}", ${req.body.total_seats}, "${req.body.color}","${req.body.model}")`
-      await con.execute(`SET FOREIGN_KEY_CHECKS=0`);
-      await con.execute(sql_statement1);
-      await con.execute(`SET FOREIGN_KEY_CHECKS=1`);
-      req.flash("success", "Registered Vehicle Successfully");
-      res.redirect("/vehicle_register");
-})
+app.get("/vehicle_register", async (req, res) => {
+  const [rows] = await con.execute(`select * from vehicle`);
+  // for(let row of rows){
+  //   console.log(row.Color)
+  // }
+  res.render("vehicles/vehicle_register.ejs", { rows });
+});
+app.post("/vehicle_register", async (req, res) => {
+  const sql_statement1 =
+    `INSERT INTO vehicle VALUES ` +
+    `("${req.body.plate_no}", ${req.body.total_seats}, "${req.body.color}","${req.body.model}")`;
+  await con.execute(`SET FOREIGN_KEY_CHECKS=0`);
+  await con.execute(sql_statement1);
+  await con.execute(`SET FOREIGN_KEY_CHECKS=1`);
+  req.flash("success", "Registered Vehicle Successfully");
+  res.redirect("/vehicle_register");
+});
 
-app.post("/student_register",catchAsync(async (req, res) => {
-    const [rows1] = await con.execute(`select * from student where CmsID=${req.body.cms}`);
-    const [rows2] = await con.execute(`select * from student where Email="${req.body.email}"`);
-    if(rows1[0]){
-        req.flash("error", "Student with this CMS ID already exists");
-        return res.redirect("/register")
+app.post(
+  "/student_register",
+  catchAsync(async (req, res) => {
+    const [rows1] = await con.execute(
+      `select * from student where CmsID=${req.body.cms}`
+    );
+    const [rows2] = await con.execute(
+      `select * from student where Email="${req.body.email}"`
+    );
+    if (rows1[0]) {
+      req.flash("error", "Student with this CMS ID already exists");
+      return res.redirect("/register");
     }
-    if(rows2[0]){
-        req.flash("error", "Student with this Email already exists");
-        return res.redirect("/register")
+    if (rows2[0]) {
+      req.flash("error", "Student with this Email already exists");
+      return res.redirect("/register");
     }
     const hash = await bcrypt.hash(req.body.password, 12);
     let sql_statement1 =
@@ -187,13 +208,17 @@ app.post("/student_register",catchAsync(async (req, res) => {
   })
 );
 
-app.post("/employee_register",catchAsync(async (req, res, next) => {
+app.post(
+  "/employee_register",
+  catchAsync(async (req, res, next) => {
     const hash = await bcrypt.hash(req.body.password, 12);
     const [rows1] = await con.execute("select max(EmpID) max_id from employee");
-    const [rows2] = await con.execute(`select * from student where Email="${req.body.email}"`);
-    if(rows2[0]){
-        req.flash("error", "Employee with this Email already exists");
-        return res.redirect("/register")
+    const [rows2] = await con.execute(
+      `select * from student where Email="${req.body.email}"`
+    );
+    if (rows2[0]) {
+      req.flash("error", "Employee with this Email already exists");
+      return res.redirect("/register");
     }
     let sql_statement1 =
       `INSERT INTO employee VALUES ` +
@@ -202,8 +227,8 @@ app.post("/employee_register",catchAsync(async (req, res, next) => {
       }", "${req.body.phnumber}"
     ,"${req.body.email}", current_date(),"${req.body.position}")`;
     let sql_statement2 =
-    `INSERT INTO login VALUES ` +
-    `("${req.body.email}", "${hash}", "Employee")`;
+      `INSERT INTO login VALUES ` +
+      `("${req.body.email}", "${hash}", "Employee")`;
     await con.execute(`SET FOREIGN_KEY_CHECKS=0`);
     await con.execute(sql_statement1);
     await con.execute(sql_statement2);
@@ -212,13 +237,19 @@ app.post("/employee_register",catchAsync(async (req, res, next) => {
     res.redirect("/home");
   })
 );
-app.post("/driver_register",catchAsync(async (req, res) => {
+app.post(
+  "/driver_register",
+  catchAsync(async (req, res) => {
     const hash = await bcrypt.hash(req.body.password, 12);
-    const [rows1] = await con.execute("select max(DriverID) max_id from driver");
-    const [rows2] = await con.execute(`select * from student where Email="${req.body.email}"`);
-    if(rows2[0]){
-        req.flash("error", "Driver with this Email already exists");
-        return res.redirect("/register")
+    const [rows1] = await con.execute(
+      "select max(DriverID) max_id from driver"
+    );
+    const [rows2] = await con.execute(
+      `select * from student where Email="${req.body.email}"`
+    );
+    if (rows2[0]) {
+      req.flash("error", "Driver with this Email already exists");
+      return res.redirect("/register");
     }
     let sql_statement1 =
       `INSERT INTO driver VALUES ` +
@@ -228,7 +259,7 @@ app.post("/driver_register",catchAsync(async (req, res) => {
     ,"${req.body.email}","${req.body.cnic}",1, "${req.body.plate_no}","${
         req.body.route_id
       }")`;
-      let sql_statement2 =
+    let sql_statement2 =
       `INSERT INTO login VALUES ` +
       `("${req.body.email}", "${hash}", "Driver")`;
     await con.execute(`SET FOREIGN_KEY_CHECKS=0`);
